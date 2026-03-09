5e0c74a626461c478e8d578d5f1aa478115e821e
Hetvaddoriya <hetvaddoriya07@gmail.com>
2026-03-09 22:23:08 +0530

    Update practical_21.java

--- practical_21.java
+++ practical_21.java
@@ -1,41 +1,36 @@
 import java.util.Scanner;
 
-public class VolumeCalculator {
+class VolumeCalculator {
 
-    // Volume of Cube
     public static double calculateVolume(double side) {
         return side * side * side;
     }
 
-    // Volume of Rectangular Cube
     public static double calculateVolume(double length, double width, double height) {
         return length * width * height;
     }
 
-    // Volume of Sphere
-    public static double calculateVolume(float radius) {
+    public static double calculateVolume(double radius, boolean isSphere) {
         return (4.0 / 3.0) * Math.PI * radius * radius * radius;
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        // Cube
         System.out.print("Enter side of cube: ");
         double side = sc.nextDouble();
         System.out.println("Volume of Cube: " + calculateVolume(side));
 
-        // Rectangular Cube
         System.out.print("\nEnter length, width, and height of rectangular cube: ");
-        double length = sc.nextDouble();
-        double width = sc.nextDouble();
-        double height = sc.nextDouble();
-        System.out.println("Volume of Rectangular Cube: " + calculateVolume(length, width, height));
+        double l = sc.nextDouble();
+        double w = sc.nextDouble();
+        double h = sc.nextDouble();
+        System.out.println("Volume of Rectangular Cube: " + calculateVolume(l, w, h));
 
-        // Sphere
         System.out.print("\nEnter radius of sphere: ");
-        float radius = sc.nextFloat();
-        System.out.println("Volume of Sphere: " + calculateVolume(radius));
+        double r = sc.nextDouble();
+        System.out.println("Volume of Sphere: " + calculateVolume(r, true));
 
         sc.close();
     }
